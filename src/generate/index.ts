a0925fddcd2842d7b5eb7c8f27dc944f4ab29ed3
Aaron Klinker <aaronklinker1@gmail.com>
2021-10-08 21:18:18 -0500

    chore: add scripts to generate code based off a local, dev image

--- src/generate/index.ts
+++ src/generate/index.ts
@@ -13,7 +13,9 @@ async function tryCatch(executor: () => Promise<void>): Promise<void> {
 }
 
 tryCatch(async () => {
-  const schema = await parseSchema('https://test.api.anime-skip.com/graphql');
+  const schema = await parseSchema(
+    `${process.env.BASE_API_URL || 'http://test.api.anime-skip.com'}/graphql`,
+  );
   const output = path.resolve(__dirname, '../index.ts');
   schema.generate(output);
 });
